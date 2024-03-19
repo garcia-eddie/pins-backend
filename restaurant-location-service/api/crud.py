16452ecaca96156b9f2d8bc4dc0f1355cadec89a
garcia-eddie <eddiegarcia.cs@gmail.com>
2024-03-18 20:26:31 -0400

    Implement endpoint to get all pins

--- restaurant-location-service/api/crud.py
+++ restaurant-location-service/api/crud.py
@@ -1,17 +1,24 @@
 from sqlalchemy.orm import Session
 from . import models, schemas
 from geoalchemy2 import WKTElement
+from typing import List
 
 
-def get_restaurant_location(db: Session, name: str):
+def get_all(db: Session) -> List[models.RLocationModel]:
+    return db.query(models.RLocationModel)
+
+
+def get_restaurant_location(db: Session, name: str) -> models.RLocationModel:
     return db.query(models.RLocationModel) \
         .filter(models.RLocationModel.name == name) \
         .first()
 
 
-def create_restaurant_location(db: Session, rLocation: schemas.RLocationBase):
+def create_restaurant_location(db: Session, rLocation: schemas.RLocationBase) -> models.RLocationModel:
     point = WKTElement(
-        f'POINT({rLocation.coordinates[0]} {rLocation.coordinates[1]})', srid=4326)
+        f'POINT({rLocation.coordinates[0]} {rLocation.coordinates[1]})',
+        srid=4326
+    )
     db_item = models.RLocationModel(name=rLocation.name, geom=point)
 
     db.add(db_item)
