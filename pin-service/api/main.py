47ce08203bcbbb529c742d0f450ccc38eb2313dc
garcia-eddie <eddiegarcia.cs@gmail.com>
2024-03-28 15:21:06 -0400

    get_db() moved to database file to prepare for router refactor

--- pin-service/api/main.py
+++ pin-service/api/main.py
@@ -4,19 +4,11 @@ from sqlalchemy.orm import Session
 
 
 from . import crud, models, schemas
-from .database import SessionLocal, engine
+from .database import engine, get_db
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
 
 
 @app.post("/maps", response_model=schemas.Map)
