b97dba6a21e1a27ffb3231d3e7ee08c389be4fea
garcia-eddie <eddiegarcia.cs@gmail.com>
2024-04-04 21:42:43 -0400

    Tests for pin endpoints added

--- pin-service/api/test_main.py
+++ pin-service/api/test_main.py
@@ -77,3 +77,35 @@ def test_get_maps(test_db):
     ]
     assert len(data) == 1
     assert data == expected
+
+
+def test_get_pins(test_db):
+    response = client.post("/maps/", json={"name": "Somerville"})
+    assert response.status_code == 200
+    data = response.json()
+    assert "id" in data
+    map_id = data["id"]
+
+    request = {
+        "name": "Ogawa",
+        "coordinates": [42.35695407894345, -71.05786815495368],
+        "map_id": map_id
+    }
+    response = client.post("/pins/", json=request)
+    assert response.status_code == 200
+    data = response.json()
+    assert "id" in data
+    pin_id = data["id"]
+
+    response = client.get('/pins/')
+    data = response.json()
+    expected = [
+        {
+            "id": pin_id,
+            "name": "Ogawa",
+            "coordinates": [42.35695407894345, -71.05786815495368],
+            "map_id": map_id
+        }
+    ]
+    assert len(data) == 1
+    assert data == expected
