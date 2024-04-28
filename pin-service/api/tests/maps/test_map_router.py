9eb4e7bb38a6bd41123d76d6e78fbbd26688d789
garcia-eddie <eddiegarcia.cs@gmail.com>
2024-04-28 16:04:57 -0400

    Refactor tests into their own submodules

---
+++ pin-service/api/tests/maps/test_map_router.py
@@ -0,0 +1,40 @@
+from api.tests.conftest import client
+
+
+def test_get_map(test_db):
+    response = client.post(
+        "/maps/",
+        json={"name": "boston"}
+    )
+    assert response.status_code == 200
+    data = response.json()
+    assert "id" in data
+    id = data["id"]
+
+    response = client.get(f'/maps/{id}')
+    expected = {
+        "id": id,
+        "name": "boston",
+        "pins": []
+    }
+    assert response.json() == expected
+
+
+def test_get_maps(test_db):
+    response = client.post("/maps/", json={"name": "boston"})
+    assert response.status_code == 200
+    data = response.json()
+    assert "id" in data
+    map_id = data["id"]
+
+    response = client.get('/maps/')
+    data = response.json()
+    expected = [
+        {
+            "id": map_id,
+            "name": "boston",
+            "pins": []
+        }
+    ]
+    assert len(data) == 1
+    assert data == expected
